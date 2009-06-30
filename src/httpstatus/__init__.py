a556bf8b6df20a9a3612a561ab880df4b55d9437
Wayne Moore <shroomling@googlemail.com>
2009-06-30 18:02:55 +0100

    Added the remaining exceptions.

--- src/httpstatus/__init__.py
+++ src/httpstatus/__init__.py
@@ -1,8 +1,31 @@
-from django.http import HttpResponseNotAllowed, HttpResponseForbidden
+from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect, \
+    HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotAllowed, \
+    HttpResponseGone
 
 
 class HTTPStatusException(Exception):
     pass
+
+
+class Http301(HTTPStatusException):
+    def __init__(self, path):
+        self.path = path
+
+    def get_response(self):
+        return HttpResponsePermanentRedirect(self.path)
+
+
+class Http302(HTTPStatusException):
+    def __init__(self, path):
+        self.path = path
+
+    def get_response(self):
+        return HttpResponseRedirect(self.path)
+
+
+class Http400(HTTPStatusException):
+    def get_response(self):
+        return HttpResponseBadRequest()
 
 
 class Http403(HTTPStatusException):
@@ -17,3 +40,7 @@ class Http405(HTTPStatusException):
     def get_response(self):
         return HttpResponseNotAllowed(self.allowed_methods)
 
+
+class Http410(HTTPStatusException):
+    def get_response(self):
+        return HttpResponseGone()
--- src/httpstatus/tests.py
+++ src/httpstatus/tests.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
+from django.http import HttpResponsePermanentRedirect
+from httpstatus import Http301
 
 class HTTPStatusTests(TestCase):
-    def test_environment(self):
-        """Just make sure everything is set up correctly."""
-        self.assert_(True)
+
+    def test_Http301(self):
+        """Test that a HttpResponsePermanentRedirect response is generated."""
+        try:
+            raise Http301('http://foo.com')
+        except Http301 as inst:
+            import pdb; pdb.set_trace()
+            self.assertEquals(HttpResponsePermanentRedirect,
+                              type(inst.get_response()))
